c0613ccffd0b73ecfda29506d94d5e7293604290
Alec74 <alecmcglone@gmail.com>
2022-03-14 18:47:16 -0400

    updated user as a secret dev user for now

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,17 +1,20 @@
 const sequelize = require('../config/connection');
 const { User, Message } = require('../models');
-// const test = require('../discordBot/testing');
+require('dotenv').config();
 
-const userData = require('./userData.json');
+// const userData = require('./userData.json');
 const allData = require('./all.json');
 
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+  User.truncate();
+  setTimeout(1000);
 
-  const users = await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
+  const users = await User.create({ 
+    name: `${process.env.DEV_USER}`,
+    email: `${process.env.DEV__EMIAL}`,
+    password: `${process.env.DEV_PASSWORD}`
   });
 
   const messages = await Message.bulkCreate(allData, {})
